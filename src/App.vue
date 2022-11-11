17aeb96d48de0b3c0d14d1374a298b1dca83321c
8-Lambda-8 <j.wasle111@gmail.com>
2022-11-12 00:41:22 +0100

    added some styling

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,20 @@
 </script>
 
 <template>
-  <div>Hello World!</div>
+  <header>
+    <h1>StorageBase</h1>
+  </header>
+  <div>
+    <nav>
+      <a href="">Parts</a>
+      <a href="categories">Categories</a>
+      <a href="storage">Storage Locations</a>
+      <a href="projects">Projects</a>
+    </nav>
+    <main>
+      <div>Hello World!</div>
+    </main>
+  </div>
 </template>
 
 <style scoped>
