787654632278e616662c23b89065160fa39776e7
8-Lambda-8 <j.wasle111@gmail.com>
2022-11-14 19:26:02 +0100

    added firebase

---
+++ src/firebase.ts
@@ -0,0 +1,27 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, enableIndexedDbPersistence } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBzZaiu-5LyOdSk8mUgOn9XSQqymu71PVk",
+  authDomain: "storagebase-4cfb2.firebaseapp.com",
+  projectId: "storagebase-4cfb2",
+  storageBucket: "storagebase-4cfb2.appspot.com",
+  messagingSenderId: "561550407112",
+  appId: "1:561550407112:web:6651c71f149c1108ef7cfd",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+enableIndexedDbPersistence(db).catch((err) => {
+  if (err.code == "failed-precondition") {
+    console.error("Multiple tabs open, persistence can only be enabled in one tab at a a time");
+  } else if (err.code == "unimplemented") {
+    console.error("The current browser does not support all of the features required");
+  }
+});
+
+//Initialize Auth
+export const auth = getAuth();
