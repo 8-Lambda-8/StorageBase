f71eb0b3b12d3c757dbd87b6891bffda52f5c166
8-Lambda-8 <j.wasle111@gmail.com>
2022-11-16 22:13:55 +0100

    implemented firebaseui in login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,13 +1,18 @@
 <template>
-  <div>
-
-  </div>
+  <div id="firebaseui-auth-container"></div>
 </template>
 
 <script setup lang="ts">
+import { onMounted } from "vue";
+import { ui, uiConfig } from "../firebase";
 
+onMounted(() => {
+  ui.start("#firebaseui-auth-container", uiConfig);
+});
 </script>
 
 <style scoped lang="scss">
-
+#firebaseui-auth-container {
+  margin: auto;
+}
 </style>
