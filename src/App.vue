3c404c16fc3d16dfe8c18ea7085ac755196324b2
8-Lambda-8 <j.wasle111@gmail.com>
2022-11-17 17:18:59 +0100

    check user and redirect login

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,22 @@
 <script setup lang="ts">
 import { onAuthStateChanged } from "@firebase/auth";
 import { onMounted, ref } from "vue";
-import { RouterLink, RouterView } from "vue-router";
+import { RouterLink, RouterView, useRouter } from "vue-router";
 import { auth } from "./firebase";
 
 const isLogedIn = ref(false);
+const router = useRouter();
 
 onMounted(() => {
   onAuthStateChanged(auth, (user) => {
     console.log("%c" + user?.displayName, "color:green; font-size:2rem");
 
     isLogedIn.value = user != null;
+    if (!user) {
+      router.push("/login");
+    } else if (router.currentRoute.value.path == "/login") {
+      router.push("/");
+    }
   });
 });
 </script>
