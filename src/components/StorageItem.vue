d59f40c77a7a72123a5794548e81bf07c31123c2
8-Lambda-8 <j.wasle111@gmail.com>
2022-11-19 00:24:52 +0100

    #3 implemented StorageLocation list

---
+++ src/components/StorageItem.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="wrapper">
+    <h4>
+      {{ storageLocation.name }}
+    </h4>
+    <div>
+      {{ storageLocation.description }}
+    </div>
+    <a :href="'/storage/edit/' + id">edit</a>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { StorageLocation } from "../types/types";
+
+defineProps<{
+  storageLocation: StorageLocation;
+  id: string;
+}>();
+</script>
+
+<style scoped lang="scss">
+h4 {
+  margin: 0.5rem;
+}
+.wrapper {
+  border: 1px black solid;
+  margin: 0.3rem 0;
+  position: relative;
+  div {
+    margin-left: 2rem;
+    margin-bottom: 0.5rem;
+  }
+  a {
+    position: absolute;
+    top: 1rem;
+    right: 1rem;
+  }
+}
+</style>
--- src/views/Storage.vue
+++ src/views/Storage.vue
@@ -3,18 +3,63 @@
     <div>
       <button @click="newStorage">New Storage Location</button>
     </div>
+    <div>
+      <StorageItem
+        v-for="storageLocationDoc in storageLocationsDocsRef"
+        :storage-location="storageLocationDoc.data()"
+        :id="storageLocationDoc.id"
+      />
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { useRouter } from "vue-router";
+import {
+  doc,
+  onSnapshot,
+  orderBy,
+  query,
+  QueryDocumentSnapshot,
+  Unsubscribe,
+  where,
+} from "firebase/firestore";
+import { onMounted, onUnmounted, ref } from "vue";
+import { StorageLocation, StorageLocationColRef } from "../types/types";
+import { auth } from "../firebase";
+import { UserColRef } from "../types/user";
+import { onAuthStateChanged } from "@firebase/auth";
+
+import StorageItem from "../components/StorageItem.vue";
 const router = useRouter();
 
 function newStorage() {
-    router.push("/storage/edit/new");
+  router.push("/storage/edit/new");
 }
 
+const storageLocationsDocsRef = ref(new Array<QueryDocumentSnapshot<StorageLocation>>());
 
+let unsubscribe: Unsubscribe;
+
+onMounted(() => {
+  onAuthStateChanged(auth, (user) => {
+    if (unsubscribe) unsubscribe();
+
+    const q = query(
+      StorageLocationColRef,
+      where("owner", "==", doc(UserColRef, user?.uid ?? "1")),
+      orderBy("name")
+    );
+
+    unsubscribe = onSnapshot(q, (storageLocationQS) => {
+      storageLocationsDocsRef.value = storageLocationQS.docs;
+    });
+  });
+});
+
+onUnmounted(() => {
+  unsubscribe();
+});
 </script>
 
 <style scoped lang="scss">
