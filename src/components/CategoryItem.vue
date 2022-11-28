23fc0479f0476e292a24536d7d569e968b363666
8-Lambda-8 <j.wasle111@gmail.com>
2022-11-28 22:50:23 +0100

    implemented CategoryItem Recursion #4

--- src/components/CategoryItem.vue
+++ src/components/CategoryItem.vue
@@ -4,16 +4,44 @@
       {{ category.name }}
     </h4>
     <a :href="'/categories/edit/' + id">edit</a>
+    <CategoryItem
+      v-for="categoryLocationDoc in categoriesDocsRef"
+      :category="categoryLocationDoc.data()"
+      :id="categoryLocationDoc.id"
+    />
   </div>
 </template>
 
 <script setup lang="ts">
-import { Category } from "../types/types";
+import { QueryDocumentSnapshot, query, where, doc, orderBy, onSnapshot } from "@firebase/firestore";
+import { Unsubscribe } from "@firebase/util";
+import { ref, onMounted, onUnmounted } from "vue";
+import { Category, CategoryColRef } from "../types/types";
 
 const props = defineProps<{
   category: Category;
   id: string;
 }>();
+
+const categoriesDocsRef = ref(new Array<QueryDocumentSnapshot<Category>>());
+
+let unsubscribe: Unsubscribe;
+
+onMounted(() => {
+  const q = query(
+    CategoryColRef,
+    where("parentCategory", "==", doc(CategoryColRef, props.id)),
+    orderBy("name")
+  );
+
+  unsubscribe = onSnapshot(q, (categoryQS) => {
+    categoriesDocsRef.value = categoryQS.docs;
+  });
+});
+
+onUnmounted(() => {
+  unsubscribe();
+});
 </script>
 
 <style scoped lang="scss">
