3e51c8335de89013c851eb2a1fa26ba01308024b
8-Lambda-8 <j.wasle111@gmail.com>
2022-11-28 21:13:31 +0100

    added "New Category" button #2

--- src/views/Categories.vue
+++ src/views/Categories.vue
@@ -1,8 +1,18 @@
 <template>
-  <div class="routerChild">Categories</div>
+  <div class="routerChild">
+    <div>
+      <button @click="newCategory">New Category</button>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
+import { useRouter } from "vue-router";
+const router = useRouter();
+
+function newCategory() {
+  router.push("/categories/edit/new");
+}
 </script>
 
 <style scoped lang="scss">
