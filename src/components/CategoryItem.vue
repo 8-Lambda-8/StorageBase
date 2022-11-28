fe7ae7cb607f75fcd4c55acff5445dc2eda61051
8-Lambda-8 <j.wasle111@gmail.com>
2022-11-28 22:48:08 +0100

    copied StorageItem to CategoryItem & made changes #4

---
+++ src/components/CategoryItem.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="wrapper">
+    <h4>
+      {{ category.name }}
+    </h4>
+    <a :href="'/categories/edit/' + id">edit</a>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Category } from "../types/types";
+
+const props = defineProps<{
+  category: Category;
+  id: string;
+}>();
+</script>
+
+<style scoped lang="scss">
+h4 {
+  margin: 0.5rem;
+}
+.wrapper {
+  border: 1px black solid;
+  margin: 0.3rem 0;
+  position: relative;
+  div {
+    margin-left: 2rem;
+    margin-bottom: 0.5rem;
+  }
+  a {
+    position: absolute;
+    top: 1rem;
+    right: 1rem;
+  }
+}
+</style>
