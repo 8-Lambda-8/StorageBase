493eb7bbc6d979a5fe6ce098e6229ca153b159a5
8-Lambda-8 <j.wasle111@gmail.com>
2022-11-29 22:31:25 +0100

    added "New Part" button #5

--- src/views/Parts.vue
+++ src/views/Parts.vue
@@ -1,8 +1,19 @@
 <template>
-  <div class="routerChild">Parts</div>
+  <div class="routerChild">
+    <div>
+      <button @click="newPart">New Part</button>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+function newPart() {
+  router.push("/parts/edit/new");
+}
 </script>
 
 <style scoped lang="scss">
