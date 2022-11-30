a3d7f7323de00869d16c974efa687b1d0617610b
8-Lambda-8 <j.wasle111@gmail.com>
2022-11-30 23:44:35 +0100

    added ParametersEditor #5

--- src/views/EditPart.vue
+++ src/views/EditPart.vue
@@ -2,31 +2,79 @@
   <div class="routerChild">
     <div class="EditPart">
       <h2>Edit Part</h2>
-      <label for="name">Name</label>
-      <input type="text" name="name" v-model="partRef.name" maxlength="20" />
-      <label for="partNr">Part Number</label>
-      <input type="text" name="partNr" v-model="partRef.partNr" />
-      <label for="description">Description</label>
-      <input type="text" name="description" v-model="partRef.description" />
-      <label for="category">Category</label>
-      <v-select
-        name="category"
-        :options="categoryTreeRef"
-        :reduce="(option:selectOptionI<Category>)=>option.docRef"
-        v-model="partRef.category"
-      />
-      <label for="footprint">Footprint</label>
-      <v-select
-        name="footprint"
-        :options="footprintRef"
-        :reduce="(option:selectOptionI<Footprint>)=>option.docRef"
-        v-model="partRef.footprint"
-      />
-      <label for="comment">Comment</label>
-      <textarea type="text" name="comment" v-model="partRef.comment"></textarea>
+      <div class="columns">
+        <div class="col">
+          <label for="name">Name</label>
+          <input type="text" name="name" v-model="partRef.name" maxlength="20" />
+          <label for="partNr">Part Number</label>
+          <input type="text" name="partNr" v-model="partRef.partNr" />
+          <label for="description">Description</label>
+          <input type="text" name="description" v-model="partRef.description" />
+          <label for="category">Category</label>
+          <v-select
+            name="category"
+            :options="categoryTreeRef"
+            :reduce="(option:selectOptionI<Category>)=>option.docRef"
+            v-model="partRef.category"
+          />
+          <label for="footprint">Footprint</label>
+          <v-select
+            name="footprint"
+            :options="footprintRef"
+            :reduce="(option:selectOptionI<Footprint>)=>option.docRef"
+            v-model="partRef.footprint"
+          />
+          <label for="comment">Comment</label>
+          <textarea type="text" name="comment" v-model="partRef.comment"></textarea>
 
-      <label for="status">Status</label>
-      <input type="text" name="status" v-model="partRef.status" />
+          <label for="status">Status</label>
+          <input type="text" name="status" v-model="partRef.status" />
+        </div>
+        <div class="col">
+          <div class="parameter">
+            <h3>Parameters:</h3>
+
+            <label for="parameterType">Add Parameter:</label>
+            <div class="addParameter">
+              <v-select name="parameterType" />
+              <button>Add</button>
+            </div>
+            <div>
+              <table>
+                <thead>
+                  <tr>
+                    <th>Name</th>
+                    <th>Value</th>
+                    <th>Tol.</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>
+                      {{ "Name" }}
+                    </td>
+                    <td>
+                      <select name="si">
+                        <option v-for="si in SiPrefixRef" :selected="si[1] == 0" :value="si">
+                          {{ si[0] }}
+                        </option>
+                      </select>
+                      <input type="number" name="value" value="10" />
+                      {{ "Ω" }}
+                    </td>
+                    <td>
+                      {{ "±" }}
+                      <input type="number" name="tolerance" value="2" />
+                      {{ "%" }}
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <div class="parameterList"></div>
+          </div>
+        </div>
+      </div>
 
       <div class="buttons">
         <button @click="ok">OK</button>
@@ -42,7 +90,7 @@ import { getAuth } from "@firebase/auth";
 import { addDoc, doc, onSnapshot, setDoc, Timestamp } from "@firebase/firestore";
 import { onMounted, ref } from "vue";
 import { useRoute, useRouter } from "vue-router";
-import { selectOptionI, categoryTreeRef, footprintRef } from "../staticLists";
+import { selectOptionI, categoryTreeRef, footprintRef, SiPrefixRef } from "../staticLists";
 import { PartI, PartColRef, PartDocRef } from "../types/part";
 import { Category, Footprint, PartParameterEntry } from "../types/types";
 import { UserColRef } from "../types/user";
@@ -114,10 +162,78 @@ function cancel() {
   margin: 0 auto;
   display: flex;
   flex-direction: column;
-  width: 20rem;
+  width: 30rem;
   textarea {
     min-height: 5rem;
     resize: vertical;
+  }
+}
+.columns {
+  display: flex;
+  flex-direction: row;
+}
+.col {
+  display: flex;
+  flex-direction: column;
+  min-width: 30%;
+  margin: 4px;
+}
+.addParameter {
+  display: flex;
+  flex-direction: row;
+  .v-select {
+    width: 100%;
+  }
+}
+.parameter > div:has(table) {
+  border-radius: 8px;
+  margin-top: 0.3rem;
+  padding: 0.5em;
+  box-sizing: border-box;
+  background-color: #ffffff55;
+  border: 2px solid #1a1a1a;
+}
+
+table {
+  color: black;
+  font-size: 0.9em;
+  box-sizing: border-box;
+  border-collapse: collapse;
+
+  th {
+    width: 33%;
+  }
+
+  th:not(:first-child),
+  td:not(:first-child) {
+    border-left: solid black 1px;
+    text-align: center;
+  }
+
+  tr:has(th) {
+    border-bottom: solid black 3px;
+  }
+
+  tr:nth-child(2) {
+    background-color: #00000033;
+  }
+
+  select {
+    margin-right: 10px;
+  }
+
+  input,
+  select {
+    background-color: transparent;
+    border: initial;
+    border-radius: 0;
+    width: 33%;
+    padding: 0;
+    text-align: right;
+  }
+  input:focus,
+  select:focus {
+    outline: none;
   }
 }
 
