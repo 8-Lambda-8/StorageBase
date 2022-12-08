5af3bb00eff7b822ccab71d3275a4d0a6b3e43b0
8-Lambda-8 <j.wasle111@gmail.com>
2022-12-08 03:42:45 +0100

    fixed frontend build + lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,7 +15,7 @@ module.exports = {
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: ["tsconfig.json"],
+    project: ["tsconfig.json", "functions/tsconfig.json"],
     sourceType: "module",
   },
   ignorePatterns: [
