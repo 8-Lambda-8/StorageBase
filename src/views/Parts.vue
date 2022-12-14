a5692ca8d2593502665ebc45cf313c46e7ae062e
8-Lambda-8 <j.wasle111@gmail.com>
2022-12-14 22:42:46 +0100

    added sidebar to show more part details #6

--- src/views/Parts.vue
+++ src/views/Parts.vue
@@ -1,28 +1,75 @@
 <template>
   <div class="routerChild">
-    <div>
-      <button @click="newPart">New Part</button>
+    <div class="tableWrapper">
+      <div>
+        <button @click="newPart">New Part</button>
+      </div>
+      <table>
+        <tr>
+          <th>Category</th>
+          <th>Name</th>
+          <th>Part Nr</th>
+          <th>Description</th>
+          <th>Footprint</th>
+          <th>Status</th>
+          <th>Coment</th>
+        </tr>
+        <tr
+          v-for="part of partsDocsRef"
+          @click="selectedPartRef = part"
+          :class="part === selectedPartRef ? 'selectedRow' : ''"
+        >
+          <td>{{ allCategories.find((c) => c.id == part.data().category.id)?.data().name }}</td>
+          <td>{{ part.data().name }}</td>
+          <td>{{ part.data().partNr }}</td>
+          <td>{{ part.data().description }}</td>
+          <td>{{ allFootprints.find((f) => f.id == part.data().footprint.id)?.data().name }}</td>
+          <td>{{ part.data().status }}</td>
+          <td>{{ part.data().comment }}</td>
+        </tr>
+      </table>
     </div>
-    <table>
-      <tr>
-        <th>Category</th>
-        <th>Name</th>
-        <th>Part Nr</th>
-        <th>Description</th>
-        <th>Footprint</th>
-        <th>Status</th>
-        <th>Coment</th>
-      </tr>
-      <tr v-for="part of partsDocsRef">
-        <td>{{ allCategories.find((c) => c.id == part.data().category.id)?.data().name }}</td>
-        <td>{{ part.data().name }}</td>
-        <td>{{ part.data().partNr }}</td>
-        <td>{{ part.data().description }}</td>
-        <td>{{ allFootprints.find((f) => f.id == part.data().footprint.id)?.data().name }}</td>
-        <td>{{ part.data().status }}</td>
-        <td>{{ part.data().comment }}</td>
-      </tr>
-    </table>
+    <div class="PartSidebar" v-if="selectedPartRef">
+      <div>
+        <h2>
+          {{ selectedPartRef.data().name }}
+        </h2>
+        <p>
+          {{ selectedPartRef.data().description }}
+        </p>
+      </div>
+
+      <table>
+        <tr>
+          <th>Category:</th>
+          <td>
+            {{
+              allCategories.find((c) => c.id == selectedPartRef?.data().category.id)?.data().name
+            }}
+          </td>
+        </tr>
+        <tr>
+          <th>Part Nr:</th>
+          <td>{{ selectedPartRef.data().partNr }}</td>
+        </tr>
+        <tr>
+          <th>Footprint:</th>
+          <td>
+            {{
+              allFootprints.find((f) => f.id == selectedPartRef?.data().footprint.id)?.data().name
+            }}
+          </td>
+        </tr>
+        <tr>
+          <th>Status:</th>
+          <td>{{ selectedPartRef.data().status }}</td>
+        </tr>
+        <tr>
+          <th>Comment:</th>
+          <td>{{ selectedPartRef.data().comment }}</td>
+        </tr>
+      </table>
+    </div>
   </div>
 </template>
 
@@ -41,6 +88,7 @@ function newPart() {
 }
 
 const partsDocsRef = ref(new Array<QueryDocumentSnapshot<Part>>());
+const selectedPartRef = ref<QueryDocumentSnapshot<Part> | null>(null);
 
 let unsubscribe: Unsubscribe;
 
@@ -58,35 +106,82 @@ onUnmounted(() => {
 </script>
 
 <style scoped lang="scss">
-table {
-  margin-top: 2rem;
-  box-sizing: border-box;
-  border-collapse: collapse;
+.routerChild {
+  flex-direction: row;
+  justify-content: space-between;
+}
 
-  td,
-  th {
-    padding: 0.3rem;
+.tableWrapper {
+  table {
+    margin-top: 2rem;
+    box-sizing: border-box;
+    border-collapse: collapse;
+    width: 100%;
+
+    td,
+    th {
+      padding: 0.3rem;
+    }
+
+    th:not(:first-child),
+    td:not(:first-child) {
+      border-left: solid gray 1px;
+    }
+
+    tr:has(th) {
+      box-shadow: inset 0 -3px gray;
+      background-color: #111;
+      position: sticky;
+      top: -1rem;
+    }
+
+    tr:nth-child(2n) {
+      background-color: #2a2a2a;
+    }
+
+    tr:has(td):hover {
+      background-color: #1c1c1c;
+    }
+    th:hover {
+      background-color: #444;
+    }
+    .selectedRow {
+      outline: 1px dotted whitesmoke;
+      background-color: #111 !important;
+    }
   }
 
-  th:not(:first-child),
-  td:not(:first-child) {
-    border-left: solid gray 1px;
+  padding-right: 1rem;
+  overflow-y: auto;
+  width: 100%;
+}
+
+.PartSidebar {
+  margin: -1rem;
+  margin-left: 0;
+  width: 300px;
+  padding: 1rem;
+  background-color: #1c1c1c;
+
+  div {
+    margin: 1rem -1rem;
+    padding: 0.25rem 1rem;
+    background-color: #111;
+    * {
+      margin: 0.5rem 0;
+    }
   }
 
-  tr:has(th) {
-    border-bottom: solid gray 3px;
-    background-color: #000000aa;
-  }
-
-  tr:nth-child(2n) {
-    background-color: #00000033;
-  }
-
-  tr:has(td):hover {
-    background-color: #00000044;
-  }
-  th:hover {
-    background-color: #ffffff44;
+  table {
+    border-collapse: collapse;
+    width: 100%;
+    th,
+    td {
+      text-align: left;
+      padding: 0.25rem;
+      width: 50%;
+      border: solid 1px #444;
+    }
   }
 }
 </style>
